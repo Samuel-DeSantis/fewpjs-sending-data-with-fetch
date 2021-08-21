90a2ad11c88847df80f5e4d56ebb982edf8036e5
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2021-08-21 15:06:27 -0600

    commit tree f4006b9265967cfabceb40f8380202d340891534

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-function submitData( name, email ) {
-    return fetch( 'http://localhost:3000/users', {
+function submitData(name, email) {
+    return fetch ('http://localhost:3000/users', {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
+            "Content-Type": "application/json",
+            "Accept": "application/json"
         },
-        body: JSON.stringify( {
-          name,
-          email
-        } )
-      } )
-      .then( function ( response ) {
-        return response.json()
-      } )
-      .then( function ( object ) {
-        document.body.innerHTML = object[ "id" ]
-      } )
-      .catch( function ( error ) {
-        document.body.innerHTML = error.message
-      } )
-  }
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then( function (response) {return response.json()})
+    .then( function (object) {document.body.innerHTML = object["id"]})
+    .catch( function (error) {document.body.innerHTML = error.message})
+}
+
+/* 
+fetch (destinationURL, configObject) 
+method <- HTTP Verb
+headers <- metadata about the data
+    'Content-Type' <- sent data format type
+    'Accept' <- tells the server what data format to accept
+body <- the data
+    JSON.stringify() converts objects to strings
+
+*/
